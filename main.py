a1743f9d014d407d00ca5de7416362f0436cfb00
SA <annso267@gmail.com>
2022-07-06 23:04:34 +0300

    modify main.py: added reading video from file, frames output

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 import argparse
+import cv2
+
 parser = argparse.ArgumentParser()
 parser.add_argument('video', type=str, help='Input dir for videos')
 
-def main ():
-    args = parser.parse_args()
+def main():
+  args = parser.parse_args()
+
+  vid_capture = cv2.VideoCapture(args.video)
+  if (vid_capture.isOpened() == False):
+    print("Error opening video file")
+  else:
+    video_fps = vid_capture.get(cv2.CAP_PROP_FPS)
+    print('FPS: ', video_fps)
+
+  print('\nPress ESC to quit\n')
+    
+  file_count = 0
+  while (vid_capture.isOpened()):
+    ret, frame = vid_capture.read()
+    if ret == True:
+      cv2.imshow('Frames', frame)
+      file_count += 1
+      key = cv2.waitKey(1)
+      if (key == 27):
+          break
+    else:
+      break
+
+  vid_capture.release()
+  cv2.destroyAllWindows()
 
 if __name__ == '__main__':
   main()
